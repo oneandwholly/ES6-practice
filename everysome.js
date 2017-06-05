a8cf9af1d5efb380ccbad3a45c87f71f821dd321
Jon Huh <oneandwholly@gmail.com>
2017-06-05 18:29:29 +0900

    add examples for every and some helper functions

---
+++ everysome.js
@@ -0,0 +1,20 @@
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted = users.every(function(user) {
+    return user.hasSubmitted;
+});
+
+
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress = requests.some(function(req) {
+  return req.status === 'pending'
+});
